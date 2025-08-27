a08932c53104c91d1978502530d5fbc6ff1faf8c
lzhgod666 <1225732228@qq.com>
2025-08-27 16:04:03 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/top/lzhgod/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lzhgod.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/main/java/top/lzhgod/gulimail/order/GulimallOrderApplication.java
@@ -0,0 +1,13 @@
+package top.lzhgod.gulimail.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallOrderApplication.class, args);
+	}
+
+}
